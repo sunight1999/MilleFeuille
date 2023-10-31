61ba0dab44dc314879d0a06c8a2bfa5c30081baa
sunight1999 <sunight1999@gmail.com>
2023-11-01 02:01:56 +0900

    IRainController를 MonoBehaviour 상속 abstract 클래스로 변경

---
+++ Assets/Scripts/Rain Controller/BasicRainController .cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class BasicRainController : RainController, ISALeftMouseClick
+{
+    ParticleSystem rain;
+
+    void Awake()
+    {
+        rain = GetComponent<ParticleSystem>();
+        rain.Stop();
+    }
+
+    public override void BeginRain()
+    {
+        rain.Play();
+    }
+
+    public override void EndRain()
+    {
+        rain.Stop();
+    }
+
+    public void OnMouseClickSignal()
+    {
+        if (rain.isPlaying)
+            rain.Stop();
+        else
+            rain.Play();
+    }
+}
